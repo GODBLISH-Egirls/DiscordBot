b864e816c06a39990f3d47d4b701d9b163dc2970
Andy Huang <41808321+Andy051@users.noreply.github.com>
2023-01-28 12:47:47 -0500

    Initial Framework commit (#3)
    * Initial Commit of starter discord bot framework

---
+++ discord_bot.py
@@ -0,0 +1,17 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run('YOUR_BOT_TOKEN')
