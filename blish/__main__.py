e7fd99d87569dd7a3009542066593a0dc0df5400
Andy Huang <41808321+Andy051@users.noreply.github.com>
2023-01-30 03:49:00 -0500

    Initial implementation of OpenAi Text Gen feature to Discord bot (#11)

--- blish/__main__.py
+++ blish/__main__.py
@@ -2,15 +2,18 @@ import asyncio
 
 from blish import bot
 from modules.sample.sample import Testing
+from modules.openai.open_ai_test import OpenAi
 from constants import BotConfig
 
 async def main() -> None:
     await bot.add_cog(Testing(bot))
+    await bot.add_cog(OpenAi(bot))
     await bot.start(BotConfig.token)
 
 if __name__=='__main__':
     try:
         asyncio.run(main())
-    except:
+    except Exception as e:
         print("ayaya something happened :<")
+        print(str(e))
         exit(69)
